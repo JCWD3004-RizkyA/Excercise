4f9d7649c1a9d79856e18e55233680055b5e920d
andiartori <rizkyandiarto@gmail.com>
2024-08-29 23:58:27 +0700

    Update Solution

---
+++ exercise10.js
@@ -0,0 +1,11 @@
+let input = true;
+let string = "string";
+let number = "number";
+
+if (string == typeof input) {
+    console.log ("1")
+} else if (number == typeof input) {
+    console.log ("2")
+} else {
+    console.log ("3")
+}
--- exercise9.js
+++ exercise9.js
@@ -1,18 +1,24 @@
 //Write a conditional statement to sort three numbers Example : num1 = 42, num2 = 27, num3 = 18 → 18, 27, 42
 
 let num1 = 42
-let num2 = 27
-let num3 = 18
+let num2 = 18
+let num3 = 27
 let Ans = [num1 , num2 , num3];
 let temp = 0;
 
+
 console.log(Ans)
 
-for (let i = 1 ; i < 3 ; i++) {
-    if (Ans[i] > Ans[i+1]) {
-        temp      = Ans[i] ;
-        Ans[i]    = Ans [i+1]
-        Ans [i+1] = temp ;
-        console.log (Ans)
+for (let h = 0 ; h < 3 ;h++) {
+
+    for (let i = 0 ; i < 3 ; i++) {
+        if (Ans[i] > Ans[i+1]) {
+            temp      = Ans[i] ;
+            Ans[i]    = Ans [i+1]
+            Ans [i+1] = temp ;
+            console.log (Ans) ;
+        }
     }
+    
 }
+
